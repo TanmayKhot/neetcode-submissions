bd3cb72084f62d7c887837dad82d91a61e8e2615
Tanmay Khot <35864383+TanmayKhot@users.noreply.github.com>
2026-06-06 12:41:35 -0700

    Add: count-number-of-islands - submission-8

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-8.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        
+        def bfs(arr, start, rows, cols):
+
+            queue = deque([start])
+            visited = {(start)}
+
+            while queue:
+                r,c = queue.popleft()
+                for dr,dc in [(1,0), (-1,0), (0,-1), (0,1)]:
+                    nr = r+dr
+                    nc = c+dc
+
+                    while 0<=nr<rows and 0<=nc<cols and arr[nr][nc] == "1" and arr[nr][nc] not in visited:
+
+                        visited.add((nr,nc))
+                        queue.append((nr,nc))
+                        arr[nr][nc] = "-1"
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        res = 0
+
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == "1":
+                    res +=1
+                    bfs(grid, (i,j), rows, cols)
+                    
+        
+        return res
+
+
+
